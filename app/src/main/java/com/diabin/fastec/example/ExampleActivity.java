0ff6a8fa22bcc2be5b06334827dd8443fb2835d5
chenguoyu313270 <2584132419@qq.com>
2018-05-28 14:13:22 +0800

    1500

--- app/src/main/java/com/diabin/fastec/example/ExampleActivity.java
+++ app/src/main/java/com/diabin/fastec/example/ExampleActivity.java
@@ -12,6 +12,6 @@ public class ExampleActivity extends ProxyActivity {
 
     @Override
     public latteDelegate setRootDelegare() {
-        return new ExampleDelagate();//测试 20180528陈国玉 1400
+        return new ExampleDelagate();//测试 20180528陈国玉 1400 1500
     }
 }
