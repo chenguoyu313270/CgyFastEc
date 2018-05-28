46d75e538262717c28a8c83d6a6650b12d9ed0ed
chenguoyu313270 <2584132419@qq.com>
2018-05-28 14:43:47 +0800

    delagate

--- app/src/main/java/com/diabin/fastec/example/ExampleDelagate.java
+++ app/src/main/java/com/diabin/fastec/example/ExampleDelagate.java
@@ -26,7 +26,7 @@ public class ExampleDelagate extends latteDelegate {
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
 
     }
-    private void testRestClient(){
+    private void testRestClient(){//ExampleDelagate
         RestClient.builder().url("")
                 .params("","")
                 .success(new ISuccess() {
