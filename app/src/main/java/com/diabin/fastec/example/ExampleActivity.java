aaee6ef105a12aecac78e9e7287f66542439a33e
chenguoyu313270 <2584132419@qq.com>
2018-05-29 10:44:29 +0800

    个人电脑提交1044

--- app/src/main/java/com/diabin/fastec/example/ExampleActivity.java
+++ app/src/main/java/com/diabin/fastec/example/ExampleActivity.java
@@ -1,4 +1,4 @@
-package com.diabin.fastec.example;
+﻿package com.diabin.fastec.example;
 
 import com.flj.latte.activities.ProxyActivity;
 import com.flj.latte.delegates.latteDelegate;
@@ -8,6 +8,6 @@ public class ExampleActivity extends ProxyActivity {
 
     @Override
     public latteDelegate setRootDelegare() {
-        return new ExampleDelagate();//测试 20180528陈国玉 1400 1500 个人电脑测试
+        return new ExampleDelagate();//测试 20180528陈国玉 1400 1500 个人电脑测试1
     }
 }
